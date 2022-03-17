feaa73bbea1521e79c3f35058a27f4e2bca5b03a
Covalent Design <98764573+covalentdesign@users.noreply.github.com>
2022-03-17 11:30:29 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
     "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
     "title": "Covalent Design NFT Dashboard",
