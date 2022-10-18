9975ebbf590a689f56705cab0e5bc214ba7efdc8
Covalent Design <98764573+covalentdesign@users.noreply.github.com>
2022-10-18 10:11:24 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x96D83bCA59508c3b11b6C16e46270a4CDB61E329",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
     "block_chain_id": "1",
